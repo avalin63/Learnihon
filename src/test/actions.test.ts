0d6a700c8bdb32d30b9be76eef74fb7ab7825e01
arthur.valin <arthur.valin@etu.uca.fr>
2023-03-22 09:39:07 +0100

    Adding actions and reducer tests

---
+++ src/test/actions.test.ts
@@ -0,0 +1,47 @@
+﻿import { Kanji } from "../model/kanji";
+import { setSelectedKanji } from "../redux/actions/setSelectedKanji";
+import { initKanjiListByGrade, KanjiListByGrade } from "../model/kanjiListByGrades";
+import { setKanjis } from "../redux/actions/setKanjis";
+import { setPlaygroundList } from "../redux/actions/setPlaygroundList";
+
+describe("test actions", () => {
+    it('should create an action with SET_SELECTED_KANJI type', () => {
+        const payload = new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+            english: "visit", japanese: "訪ねる（たずねる）"
+            }]);
+        const expectation = {
+            type: 'SET_SELECTED_KANJI',
+            payload: payload.toObject(),
+        };
+
+        expect(setSelectedKanji(payload)).toEqual(expectation);
+    });
+
+    it('should create an action with SET_KANJIS type', () => {
+        const payload: KanjiListByGrade = initKanjiListByGrade();
+        const expectation = {
+            type: 'SET_KANJIS',
+            payload: payload,
+        };
+
+        expect(setKanjis(payload)).toEqual(expectation);
+    });
+
+    it('should create an action with SET_PLAYGROUND_LIST type', () => {
+        const payload: Kanji[] = [
+            new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+                english: "visit", japanese: "訪ねる（たずねる）"
+            }]),
+            new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+                english: "visit", japanese: "訪ねる（たずねる）"
+            }])
+        ];
+        const expectation = {
+            type: 'SET_PLAYGROUND_LIST',
+            payload: payload,
+        };
+
+        expect(setPlaygroundList(payload)).toEqual(expectation);
+    });
+
+});
---
+++ src/test/reducer.test.ts
@@ -0,0 +1,74 @@
+﻿import { Kanji } from "../model/kanji";
+import { initKanjiListByGrade, KanjiListByGrade } from "../model/kanjiListByGrades";
+import kanjiReducer from "../redux/reducers/kanjiReducer";
+
+describe('test reducer', () => {
+
+    let initialState = {
+        kanjis: initKanjiListByGrade(),
+        selectedKanji: null,
+        playgroundList: []
+    }
+
+    it('should return initial state', () => {
+        expect(kanjiReducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should handle SET_SELECTED_KANJI', () => {
+        const kanji = new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+            english: "visit", japanese: "訪ねる（たずねる）"
+        }]);
+        expect(
+            kanjiReducer(initialState, {
+                type: 'SET_SELECTED_KANJI',
+                payload: kanji
+            })
+        ).toEqual({
+            kanjis: initKanjiListByGrade(),
+            selectedKanji: kanji,
+            playgroundList: []
+        });
+    });
+
+    it('should handle SET_KANJIS', () => {
+        const kanjis: KanjiListByGrade = initKanjiListByGrade();
+        kanjis["Grade 1"] = [
+            new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+                english: "visit", japanese: "訪ねる（たずねる）"
+            }]),
+            new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+                english: "visit", japanese: "訪ねる（たずねる）"
+            }])];
+        expect(
+            kanjiReducer(initialState, {
+                type: 'SET_KANJIS',
+                payload: kanjis
+            })
+        ).toEqual({
+            kanjis: kanjis,
+            selectedKanji: null,
+            playgroundList: []
+        });
+    });
+
+    it('should handle SET_PLAYGROUND_LIST', () => {
+        const kanjis = [
+            new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+                english: "visit", japanese: "訪ねる（たずねる）"
+            }]),
+            new Kanji("訪", "visit", "", "", 11, "ホウ", "おとずれる", { character: "", position: "" }, [{
+                english: "visit", japanese: "訪ねる（たずねる）"
+            }])];
+        expect(
+            kanjiReducer(initialState, {
+                type: 'SET_PLAYGROUND_LIST',
+                payload: kanjis
+            })
+        ).toEqual({
+            kanjis: initKanjiListByGrade(),
+            selectedKanji: null,
+            playgroundList: kanjis
+        });
+    });
+
+});
